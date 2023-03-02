00d84eea8e8a62f2aff9f3f6bc342cf724377a8f
Caps Lock <46106832+capsey@users.noreply.github.com>
2023-03-03 00:34:17 +0600

    Tests improved

--- tests/main.c
+++ tests/main.c
@@ -2,59 +2,73 @@
 #include "sput.h"
 #include <stdio.h>
 
+#define check_valid(input, expected)                                                             \
+    do {                                                                                         \
+        sput_fail_unless(mx_eval(config, input, &result) == MX_SUCCESS, "No evaluation errors"); \
+        sput_fail_unless(result == (expected), "Correct result expected");                       \
+    } while (0)
+
+#define check_valid_literal(input) check_valid(#input, input)
+
+#define check_invalid(input, error)                                                     \
+    do {                                                                                \
+        sput_fail_unless(mx_eval(config, input, &result) == error, #error " expected"); \
+    } while (0)
+
 static void test_eval_valid() {
-    mx_config *config;
+    mx_config *config = mx_init_simple();
     double result;
 
-    // Simple arithmetic operators
-    config = mx_init_simple();
+    // Integers
+    check_valid_literal(25);
+    check_valid_literal(1 - 1);
+    check_valid_literal(5 + 5);
+    check_valid("6 * 18 / 2", 54);
 
-    sput_fail_unless(mx_eval(config, "25", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == 25, "Incorrect result");
+    // Rational
+    check_valid_literal(3.54);
+    check_valid_literal(.8);
+    check_valid("5 / 2", 2.5);
+    check_valid("1 / 3", 1.0 / 3);
 
-    sput_fail_unless(mx_eval(config, "1 + 1", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == 2, "Incorrect result");
+    // Grouping (parentheses)
+    check_valid_literal((1 + 2) / 3);
+    check_valid_literal((9 * 2.12) / 1.8);
 
-    sput_fail_unless(mx_eval(config, "5 + 5", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == 10, "Incorrect result");
+    mx_free(config);
+}
 
-    sput_fail_unless(mx_eval(config, "6 * 18 / 2", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == 54, "Incorrect result");
+static void test_eval_corner_valid() {
+    mx_config *config = mx_init_simple();
+    double result;
 
-    sput_fail_unless(mx_eval(config, "5 / 2", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == 2.5, "Incorrect result");
-
-    sput_fail_unless(mx_eval(config, "1 / 3", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == (1.0 / 3.0), "Incorrect result");
-
-    sput_fail_unless(mx_eval(config, "(1 + 2) / 3", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == 1, "Incorrect result");
-
-    sput_fail_unless(mx_eval(config, ".8", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == 0.8, "Incorrect result");
-
-    sput_fail_unless(mx_eval(config, "(9 * 2.12) / 1.8", &result) == MX_SUCCESS, "Evaluation failed");
-    sput_fail_unless(result == ((9 * 2.12) / 1.8), "Incorrect result");
+    // Implicit parentheses
+    check_valid("5 + 4) *2", 18);
 
     mx_free(config);
 }
 
 static void test_eval_invalid() {
-    mx_config *config;
+    mx_config *config = mx_init_simple();
     double result;
 
-    // Simple arithmetic operators
-    config = mx_init_simple();
+    // Incomplete operators
+    check_invalid("1 +", MX_SYNTAX_ERROR);
+    check_invalid("* 1", MX_SYNTAX_ERROR);
+    check_invalid("* 1", MX_SYNTAX_ERROR);
 
-    sput_fail_unless(mx_eval(config, "1 +", &result) == MX_SYNTAX_ERROR, "Evaluation succeeded");
-    sput_fail_unless(mx_eval(config, "* 1", &result) == MX_SYNTAX_ERROR, "Evaluation succeeded");
-    sput_fail_unless(mx_eval(config, "* 1", &result) == MX_SYNTAX_ERROR, "Evaluation succeeded");
-    sput_fail_unless(mx_eval(config, "5 5", &result) == MX_SYNTAX_ERROR, "Evaluation succeeded");
-    sput_fail_unless(mx_eval(config, "x", &result) == MX_UNDEFINED, "Evaluation succeeded");
-    sput_fail_unless(mx_eval(config, ")", &result) == MX_SYNTAX_ERROR, "Evaluation succeeded");
-    sput_fail_unless(mx_eval(config, "(", &result) == MX_SYNTAX_ERROR, "Evaluation succeeded");
-    sput_fail_unless(mx_eval(config, "()", &result) == MX_SYNTAX_ERROR, "Evaluation succeeded");
-    sput_fail_unless(mx_eval(config, "", &result) == MX_SYNTAX_ERROR, "Evaluation succeeded");
+    // Double values
+    check_invalid("10 5", MX_SYNTAX_ERROR);
+    check_invalid("5, 5", MX_SYNTAX_ERROR);
+
+    // Undefined identifiers
+    check_invalid("x", MX_UNDEFINED);
+
+    // Empty expression
+    check_invalid(")", MX_SYNTAX_ERROR);
+    check_invalid("(", MX_SYNTAX_ERROR);
+    check_invalid("()", MX_SYNTAX_ERROR);
+    check_invalid("", MX_SYNTAX_ERROR);
 
     mx_free(config);
 }
@@ -62,13 +76,16 @@ static void test_eval_invalid() {
 int main() {
     sput_start_testing();
 
+    // mx_eval(...)
     sput_enter_suite("mx_eval(...): Valid input");
     sput_run_test(test_eval_valid);
+
+    sput_enter_suite("mx_eval(...): Valid input corner cases");
+    sput_run_test(test_eval_corner_valid);
 
     sput_enter_suite("mx_eval(...): Invalid input");
     sput_run_test(test_eval_invalid);
 
     sput_finish_testing();
-
     return sput_get_return_value();
 }
