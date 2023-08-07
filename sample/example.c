884ac23507b487e118f3de9b374a8409a8d2896d
Caps Lock <46106832+capsey@users.noreply.github.com>
2023-08-07 21:29:35 +0600

    chore(readme): update README.md to include new C++ interface

---
+++ sample/example.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <mathex.h>
+
+int main()
+{
+    // Use `mx_create` and `MX_DEFAULT` to get default settings.
+    // For what settings are available, check out documentation.
+    mx_config *config = mx_create(MX_DEFAULT);
+
+    // Config that you get from `mx_create` contains your settings
+    // along with custom variables and functions you inserted.
+    double x = 1.5;
+    mx_add_variable(config, "x", &x);
+
+    // These variables and functions are then available for users
+    // to use in expressions.
+    char *input = "2x + 5";
+    double result;
+
+    // Mathex writes result of evaluation into the address you provide.
+    // Here we pass an address to a `result` variable we declared above.
+    if (mx_evaluate(config, input, &result) == MX_SUCCESS)
+    {
+        printf("%s is %.0lf\n", input, result); // Outputs `2x + 5 is 8`
+    }
+
+    // Don't forget to free the memory once you are done.
+    mx_free(config);
+}
